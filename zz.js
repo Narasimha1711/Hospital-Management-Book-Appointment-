3f2a04c244ee4a2e1b8c7c365de873d850154083
Narasimha1711 <rockynarasimha17@gmail.com>
2024-05-03 16:38:23 +0530

    total

--- zz.js
+++ zz.js
@@ -1,30 +1,14 @@
-const bcrypt = require('bcrypt')
-const saltRounds = 10;
+// const bcrypt = require('bcrypt')
+// const saltRounds = 10;
 
-myPlaintextPassword = "narasimha";
+// myPlaintextPassword = "narasimha";
 
-let a;
-
-// bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
-//     // Store hash in your password DB.
-
-//     // console.log(myPlaintextPassword)
-//     console.log(hash)
-
-//     a = hash;
-
-    
-// });
+// let a;
 
 
-// console.log(hash)
-const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
 
-bcrypt.compareSync(myPlaintextPassword, hash);
-console.log(bcrypt.compareSync(myPlaintextPassword, hash))
 
-// bcrypt.compare(myPlaintextPassword, a, function(err, result) {
-//     // result == true
-//     console.log("true")
-//     console.log(a)
-// });
+// const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+
+// bcrypt.compareSync(myPlaintextPassword, hash);
+// console.log(bcrypt.compareSync(myPlaintextPassword, hash))
